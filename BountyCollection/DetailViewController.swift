a8ab7555814324a159b63920700c91db0b1f5c07
SIRIUS KIM <jmoumnet@gmail.com>
2023-11-17 16:22:06 +0900

    231117 Toy Project - BountyCollection
    1. Animation 적용
    2. Constraint를 IBOulet으로 연결하여 직접 설정
    3. View의 속성 (position, size, transformation, appearance 등등)을 사용하여 animate

--- BountyCollection/DetailViewController.swift
+++ BountyCollection/DetailViewController.swift
@@ -13,11 +13,46 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
     
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLableCenterX: NSLayoutConstraint!
+    
     let viewModel = DetailViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        prepareAnimation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAnimation()
+    }
+    
+    func prepareAnimation() {
+//        nameLabelCenterX.constant = view.bounds.width
+//        bountyLableCenterX.constant = view.bounds.width
+        
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
+    }
+    
+    func showAnimation() {
+//        nameLabelCenterX.constant = 0
+//        bountyLableCenterX.constant = 0
+//        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: { self.view.layoutIfNeeded() }, completion: nil)
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+            self.nameLabel.alpha = 1
+        }, completion: nil)
+        UIView.animate(withDuration: 1, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.alpha = 1
+        }, completion: nil)
+        UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
     }
     
     func updateUI() {
